29c0f3e383972dfd646964d50a034e44a33d6a7a
Birdgeek <birdgeekthree@gmail.com>
2020-07-17 13:10:59 -0600

    V0.1 - Intial
    Added System & AMP Update functions

--- Amp/ampupgrade.sh
+++ Amp/ampupgrade.sh
@@ -1,14 +1,83 @@
 #!/bin/bash
-clear
 
-echo "Upgrading AMP script v0.1"
+#Functions
+check_exit_status() {
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo su -l amp
-ampinstmgr upgradeall
-ampinstmgr startall
-exit
-echo "***********************"
-echo "* Completed Amp Update*"
-echo "***********************"
+	if [ $? -eq 0 ]
+	then
+		echo
+		echo "Success"
+		echo
+	else
+		echo
+		echo "[ERROR] Process Failed!"
+		echo
+		
+		read -p "The last command exited with an error. Exit script? (yes/no) " answer
+
+            if [ "$answer" == "yes" ]
+            then
+                exit 1
+            fi
+	fi
+}
+
+greeting() {
+
+	echo
+	echo "Hello, $USER. Running AMP Auto-Updater v0.1"
+	echo
+}
+
+update() {
+
+    sudo apt-get update;
+	check_exit_status
+
+    sudo apt-get upgrade -y;
+	check_exit_status
+}
+
+housekeeping() {
+
+	sudo apt-get autoremove -y;
+	check_exit_status
+
+	sudo apt-get autoclean -y;
+	check_exit_status
+
+	sudo updatedb;
+	check_exit_status
+}
+
+endSystemUpdate() {
+
+	echo
+	echo "---------------------------"
+	echo "- System Update Complete! -"
+	echo "---------------------------"
+	echo
+}
+
+ampUpdate() {
+	sudo su -l amp
+	ampinstmgr upgradeall
+	ampinstmgr startall
+}
+
+exitScript() {
+	echo
+	echo "------------------------"
+	echo "- AMP Update Completed -"
+	echo "------------------------"
+	echo
+	exit
+}
+
+#Script
+greeting
+update
+housekeeping
+endSystemUpdate
+ampUpdate
+exitScript
