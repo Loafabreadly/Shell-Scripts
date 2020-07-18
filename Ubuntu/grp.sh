0046e578a46930cf896ec430cb51519bb654e100
Birdgeek <birdgeekthree@gmail.com>
2020-07-18 09:26:59 -0600

    [V0.4] GRP + Install Script

---
+++ Ubuntu/grp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#GRP = Git Reset Pull
+# v0.1
+cd Shell-Scripts/
+sudo git reset --hard
+sudo git pull
+sudo chmod +x -R ../Shell-Scripts/
+cd ~
---
+++ Ubuntu/installScripts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Install Scripts Scipt
+#v0.1
+if [ -d "~/bin" ]
+then
+	startInstall
+else
+	mkdir ~/bin
+fi
+
+	startInstall
+
+startInstall () {
+	
+	cd ~/Shell-Scipts/
+	sudo cp Amp/ampupgrade.sh ~/bin/ampup
+	sudo cp Amp/ampdeploy.sh ~/bin/ampdeploy
+	sudo cp PiHole/piup.sh ~/bin/piup
+	sudo cp Ubuntu/grp.sh ~/bin/grp
+	ls -l ~/bin
+	export PATH=~/bin:$PATH
+	source ~/.bashrc
+}
